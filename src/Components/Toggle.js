2a7ae908f04f1b1673c0e2a8e1b5271bcb02b928
MohammedAli144 <1411mody@gmail.com>
2025-01-25 15:03:33 +0200

    this is por

--- src/Components/Toggle.js
+++ src/Components/Toggle.js
@@ -1,4 +1,3 @@
-
 import { Link } from 'react-router-dom';
 import style from '../Toggle.module.css'
 export default function Toggle() {
@@ -6,7 +5,7 @@ export default function Toggle() {
         <div className='mt-3 d-flex justify-content-end bgLink d-flex container'>
             <Link to='/' className={style.toggle}> Home</Link>
             <Link to="Resume" className={style.toggle}> Resume</Link>
-            <Link to="Works" className={style.toggle}> Work</Link>
+            <Link to="Works" className={style.toggle}> Works</Link>
             <Link to="Contact" className={style.toggle}> Contact</Link>
         </div>
     );
