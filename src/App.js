c236df093d8f2e08806fe0a19bf72437a482dfa9
MohammedAli144 <1411mody@gmail.com>
2025-01-27 23:48:35 +0200

    edit toggle js and app js and toggle module css fils

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import Contact from "./Components/Content/Contact";
-import Home from "./Components/Content/Home";
-import Resume from "./Components/Content/Resume";
-import Works from "./Components/Content/Works";
+
 import Navbar from "./Components/Navbar";
 import Data from "./Components/perData/Data";
 import Toggle from "./Components/Toggle";
-import './App.css'
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-
-
+import './App.css';
+import { BrowserRouter as Router } from "react-router-dom";
+import ContentApp from "./Components/Content/ContentApp";
 function App() {
-
   return (
     <div className="App bg-light">
       <Router>
@@ -23,14 +18,8 @@ function App() {
               <Data />
             </div>
             <div className="col-lg-9 col-md-8 col-sm-12 bg-white p-3">
-              <Routes>
-                <Route path="/" element={<Home/> } />
-                <Route path="/Resume" element={<Resume/> } />
-                <Route path="/Works" element={<Works/> } />
-                <Route path="/Contact" element={<Contact/> } />
-              </Routes>  
+              <ContentApp />
             </div>
-
           </div>
         </div>
       </Router>
--- src/Components/Toggle.js
+++ src/Components/Toggle.js
@@ -3,10 +3,33 @@ import style from '../Toggle.module.css'
 export default function Toggle() {
     return (
         <div className='mt-3 d-flex justify-content-end bgLink d-flex container'>
-            <Link to='/' className={style.toggle}> Home</Link>
-            <Link to="Resume" className={style.toggle}> Resume</Link>
-            <Link to="Works" className={style.toggle}> Works</Link>
-            <Link to="Contact" className={style.toggle}> Contact</Link>
+            <div className='d-none d-sm-block'>
+                <Link to='/' className={style.toggle}> Home</Link>
+                <Link to="Resume" className={style.toggle}> Resume</Link>
+                <Link to="portfolio" className={style.toggle}> portfolio</Link>
+                <Link to="Contact" className={style.toggle}> Contact</Link>
+            </div>
+            
+            <div className='d-flex d-sm-none justify-content-between '>
+                <div className={style.mobile}>
+                    <Link to='/'>
+                        <i className="fa-solid fa-house"></i>
+                        <span>Home</span>
+                    </Link>
+                    <Link to="Resume" className=''>
+                        <i className="fa-solid fa-laptop-code"></i>
+                        Resume
+                    </Link>
+                    <Link to="portfolio" className=''>
+                        <i className="fa-solid fa-folder-open"></i>
+                        <span>portfolio</span>
+                    </Link>
+                    <Link to="Contact" className=''>
+                        <i className="fa-solid fa-phone"></i>
+                        Contact
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 }
